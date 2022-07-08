a71d154c5d79e58372ed9bc64c4e5f9a1a1d03f2
Kristina Brennan <96805863+thetinaest@users.noreply.github.com>
2022-07-07 20:10:29 -0500

    State and fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react'
+
 import Header from './components/header';
 import SearchForm from './components/searchForm';
 import PokemonDetails from './components/pokemonDetails';
 import Spinner from './components/spinner';
 
 function App() {
+  const [pokemon, setPokemon] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('ninetales')
+
+  useEffect(() => {
+    getPokemon()
+  }, [])
+
+  const getPokemon = () => {
+    setLoading(true)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
+      .then(res => res.json())
+      .then(json => setPokemon(json))
+      .catch(err => setError('Pokemon not found'))
+      .finally(() => setLoading(false))
+  }
+
   return (
     <>
       <Header />
